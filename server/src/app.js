fe5cc4785c76936026669ecd4756785fc0c05787
Anowar Hasan Sabir <sujon@Anowars-Mac-mini.local>
2020-03-09 07:14:20 +0600

    setup of the express js server

---
+++ server/src/app.js
@@ -0,0 +1,16 @@
+//  console.log('hello world!!!!')
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+
+const app = express()
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+app.use(cors())
+
+app.get('/register', (req, res) => {
+  res.send({ message: 'your user was registered!! ' })
+})
+
+app.listen(process.env.PORT || 8081)
